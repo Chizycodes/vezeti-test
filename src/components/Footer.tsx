7b7170b29c316b95f33f7814e0432ac39d48939f
Chizycodes <okwudirejoy@gmail.com>
2023-01-29 01:21:04 +0100

    footer implementation

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,32 @@
-import React, { FC } from 'react';
+import React from 'react';
 
-interface Props {}
+const Footer = () => {
+	return (
+		<footer className="max-w-[1280px] mx-auto px-5 flex flex-col items-center">
+			<div className="flex items-center gap-5 md:gap-10 mb-[35px]">
+				<a href="https://facebook.com" target="_blank" rel="noreferrer">
+					<img src="/images/fb.svg" alt="fb" />
+				</a>
+				<a href="https://instagram.com" target="_blank" rel="noreferrer">
+					<img src="/images/instagram.svg" alt="instagram" />
+				</a>
+				<a href="https://twitter.com/chizycodes" target="_blank" rel="noreferrer">
+					<img src="/images/twitter.svg" alt="twitter" />
+				</a>
+				<a href="https://youtube.com" target="_blank" rel="noreferrer">
+					<img src="/images/youtube.svg" alt="youtube" />
+				</a>
+			</div>
 
-const Footer: FC<Props> = () => {
-	return <div>Header</div>;
+			<div className="flex items-center gap-5 md:gap-10 mb-[35px] text-[#111827] font-bold text-xs md:text-[18px]">
+				<a href="/">Conditions of Use</a>
+				<a href="/">Privacy & Policy</a>
+				<a href="/">Press Room</a>
+			</div>
+
+			<p className="text-[#6B7280] text-xs md:text-[18px] font-bold pb-10">&copy; 2021 Vezeti</p>
+		</footer>
+	);
 };
 
 export default Footer;
